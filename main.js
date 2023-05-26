d02348d517530f99fcf223636407723de936e02b
Paulo Marinato <marinatopaulo@gmail.com>
2023-05-25 21:31:47 -0300

    modificação Stiles

---
+++ main.js
@@ -0,0 +1,30 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* Obtém o botão e o elemento de exibição do número
+var gerarNumeroBtn = document.getElementById('gerarNumeroBtn');
+var numeroGerado = document.getElementById('numeroGerado');
+
+// Adiciona um listener de clique ao botão
+gerarNumeroBtn.addEventListener('click', function() {
+  // Gera um número aleatório entre 1 e 100
+  var numero = Math.floor(Math.random() * 50) + 1;
+  
+  // Exibe o número gerado
+  numeroGerado.textContent = "Número Gerado: " + numero;
+});*/
